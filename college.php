0e87c8d6baa16ffb22eaadc50722e84865da6fd0
Namitagr1 <64659297+Namitagr1@users.noreply.github.com>
2022-10-15 17:29:45 -0400

    Delete college.php

--- college.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-
-$results = shell_exec("python college-finder.py " . $_POST['state'] . ' ' . $_POST['zipcode'] . ' ' . $_POST['degree'] . ' ' . $_POST['SAT/ACT'] . ' ' . $_POST['Score'] . ' 2>&1');
-
-echo $results;
-
-?>
